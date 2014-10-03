2d15c2d0cf3c6d3618dd72f489bfa3d6bb384556
TheZallen <zak.allen1009@gmail.com>
2014-10-02 22:55:06 -0500

    Initial Commit

---
+++ GazeFocus/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  GazeFocus
+//
+//  Created by Zak Allen on 10/2/14.
+//  Copyright (c) 2014 Tuko Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
